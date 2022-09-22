1e6a7dd619dc4de7594691df967c1ce0a650d9c7
Miguel Ángel Latre Abadía <latre@unizar.es>
2022-09-22 13:14:27 +0200

    Actualización al curso 2022-23
    Retirada de versiones con funciones y renumeración

--- 6-factorial-version-for.cpp
+++ 6-factorial-version-for.cpp
@@ -1,29 +1,13 @@
 /*******************************************************************************
  * Curso de Programación 1. Tema 4
  * Autores: Miguel Ángel Latre y Javier Martínez
- * Última revisión: 2 de octubre de 2020
+ * Última revisión: 22 de septiembre de 2022
  * Resumen: Ejemplo de un programa que utiliza composición iterativa para 
  *          calcular el factorial de un número. 
- *          Versión con una función «factorial» independiente 
- *          (ver tema 5 del curso).
+ *          Versión con bucle «for».
  ******************************************************************************/
 #include <iostream>
 using namespace std;
-
-/*
- * Devuelve n!
- */
-unsigned factorial(unsigned n) {
-    // Asigna a «factorial» el valor de «n»!, siendo n>=0
-    unsigned i = 1;
-    unsigned factorial = 1;         // factorial = i!
-    while (i < n) {
-        i++;
-        factorial = i * factorial;      // factorial = i!
-    }
-    // i = n, factorial = i! ==> factorial = n!
-    return factorial;
-}
 
 /*
  * Programa que pide al usuario un número natural, lo lee del teclado y escribe
@@ -33,8 +17,13 @@ int main() {
     cout << "Escriba un número natural: ";
     unsigned n;
     cin >> n;
+
+    // Asigna a «factorial» el valor de «n»!, siendo n>=0
+    unsigned factorial = 1;                  // factorial = i!
+    for (unsigned i = 1; i <= n; i++) {
+        factorial = i * factorial;      // factorial = i!
+    }
+    // i = n, factorial = i! ==> factorial = n!
     
-    cout << n << "! = " << factorial(n) << endl;
-    
-    return 0;
+    cout << n << "! = " << factorial << endl;
 }
